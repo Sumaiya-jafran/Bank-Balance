a722ccc833fc6f848b9115c98defb39f955cfdfe
sumaiya ahmed <sumu0612@gmail.com>
2022-08-17 03:08:24 +0600

    2nd  commit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //step-1: add click handler with  the submit btn
 
-document.getElementById('submit-btn').addEventListener('click',function(event){
+document.getElementById('submit-btn').addEventListener('click',function(){
     //step:2 Get the email and password input field
     const emailField = document.getElementById('email-field');
     const passwordField = document.getElementById('password-field');
     const email = emailField.value;
     const password = passwordField.value;
     // console.log(email,password)
-    if(email === "ami@gmail.com" && password === "tumi"){
+    if(email === "ami@gmail.com" && password === "itsme"){
         console.log('valid User');
         window.location.href = 'bank.html'
     }
-    else
-    alart("Invalid User")
+    else {
+        console.log(`invalid`)
+        alert(`email:  ami@gmail.com  pass: itsme`);
+    }
+    
 })
